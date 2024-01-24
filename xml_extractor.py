771d3a06355cc0d672fc06d2495a5de700bfede9
Adam.Ozeri <Adam.Ozeri@s.afeka.ac.il>
2024-01-24 17:20:22 +0200

    Created xml_extractor script

---
+++ xml_extractor.py
@@ -0,0 +1,22 @@
+import gzip
+import os
+import sys
+import shutil
+
+
+def unzip_xml_files(directory, file):
+    zip_path = os.path.join(directory, file)
+    xml_file_name = file.split('.gz')[0] + '.xml'
+    xml_path = os.path.join('./xml_files', xml_file_name)
+    with gzip.open(zip_path, 'rb') as f_in, open(xml_path, 'wb') as f_out:
+        shutil.copyfileobj(f_in, f_out)
+
+
+def main():
+    zip_directory = sys.argv[1]
+    for filename in os.listdir(zip_directory):
+        unzip_xml_files(zip_directory, filename)
+
+
+if __name__ == '__main__':
+    main()
